a993dc8c2d70e4701dfdcfaa74a33730a4cbe961
Jamie Bryson <jamieab98@gmail.com>
2025-10-17 18:11:58 -0400

    Created the NewProject css module and started styling the title portion and the description portions

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,20 @@
+import styles from './NewProject.module.css'
+
 
 function NewProject() {
     return(
-        <>
-            <div>NewProject Component</div>
-        </>
+        <div className={styles.NewProject}>
+            <h3>Add Your Project</h3>
+            <h4>Project Title</h4>
+            <form>
+                <input type="text" placeholder='Title of Project' className={styles.titleform}></input>
+            </form>
+            <div>Description of Project</div>
+            <form>
+                <textarea placeholder="Description of the project" className={styles.descriptionform}></textarea>
+            </form>
+            <div>Button to add Project</div>
+        </div>
     )
 }
 
