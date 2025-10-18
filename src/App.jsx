ab87ec3733a16d396b046979df9ab992c247dff7
Jamie Bryson <jamieab98@gmail.com>
2025-10-18 11:50:43 -0400

    Working on styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <>
+    <div className={styles.App}>
       <Header/>
       <Body/>
-    </>
+    </div>
   )
 }
 
