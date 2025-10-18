84feb74bf8df83edc533fb94fde20b9921cdf339
Jamie Bryson <jamieab98@gmail.com>
2025-10-18 10:27:46 -0400

    Added the button to submit the project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -5,15 +5,13 @@ function NewProject() {
     return(
         <div className={styles.NewProject}>
             <h3>Add Your Project</h3>
-            <h4>Project Title</h4>
             <form>
+                <div>Project Title</div>
                 <input type="text" placeholder='Title of Project' className={styles.titleform}></input>
-            </form>
-            <div>Description of Project</div>
-            <form>
+                <div>Description of Project</div>
                 <textarea placeholder="Description of the project" className={styles.descriptionform}></textarea>
+                <button type="submit">Add Project</button>
             </form>
-            <div>Button to add Project</div>
         </div>
     )
 }
