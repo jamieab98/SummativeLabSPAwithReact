5d4fe04a7bf2d32e76df6f2e6b92021d61505c1c
Jamie Bryson <jamieab98@gmail.com>
2025-10-19 10:23:23 -0400

    Worked on styling of the projects. Looks pretty good imo

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,15 +1,16 @@
 import projectData from "../projectData"
+import styles from "./ProjectCards.module.css"
 
 function ProjectCards() {
     return(
         <>
             {projectData.map((project) => (
-                <div>
+                <div className={styles.project}>
                     <div>
-                        <button>Click to go to the project</button>
+                        <button className={styles.projectButton}>Click to go to the project</button>
                     </div>
-                    <div>
-                        <h4>{project.title}</h4>
+                    <div className={styles.projectText}>
+                        <h4 className={styles.projectTitle}>{project.title}</h4>
                         <div>{project.description}</div>
                     </div>
                 </div>
--- src/projectData.js
+++ src/projectData.js
@@ -1,7 +1,7 @@
 const projectData = [
     {title: "Project 1", description: "Description of Project 1"},
     {title: "Project 2", description: "Description of Project 2"},
-    {title: "Project 3", description: "Description of Proejct 3"}
+    {title: "Project 3", description: "Description of Project 3"}
 ]
 
 export default projectData;
