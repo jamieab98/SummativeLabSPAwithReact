853bb0619ae0fda1e3913eaeb83cfda565494d50
Jamie Bryson <jamieab98@gmail.com>
2025-10-20 17:34:13 -0400

    Styled the link input to match the other user inputs

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -10,6 +10,8 @@ function NewProject({submitForm, updateTitle, updateDescription, newTitle, newDe
                 <input type="text" onChange={updateTitle} placeholder='Title of Project' id="projectTitle" value={newTitle} className={styles.titleform}></input>
                 <label htmlFor="projectDescription">Description of Project</label>
                 <textarea onChange={updateDescription} placeholder="Description of the project" id="projectDescription" value={newDescription}className={styles.descriptionform}></textarea>
+                <label htmlFor="link">Link</label>
+                <input type="text" placeholder="Link to acces the Project" id="link" className={styles.linkform}></input>
                 <button type="submit">Add Project</button>
             </form>
         </div>
