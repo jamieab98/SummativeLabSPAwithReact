eeadf3b1e4796e571b5f2a9473241d75480b738a
Jamie Bryson <jamieab98@gmail.com>
2025-10-20 17:30:13 -0400

    Made it so the button will open the link in a new page

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,12 +1,13 @@
 import styles from "./ProjectCards.module.css"
 
 function ProjectCards({data}) {
+
     return(
         <>
             {data.map((project, index) => (
                 <div className={styles.project} key={index}>
                     <div>
-                        <button className={styles.projectButton}>Click to go to the project</button>
+                        <a href={project.link} target="_blank"><button className={styles.projectButton}>Click to go to the project</button></a>
                     </div>
                     <div className={styles.projectText}>
                         <h4 className={styles.projectTitle}>{project.title}</h4>
