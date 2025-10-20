2f76ba231789d5279302bdd7c089d6fe3810c30d
Jamie Bryson <jamieab98@gmail.com>
2025-10-20 17:19:30 -0400

    Updated the projectData.js file to include a link to the projects

--- src/projectData.js
+++ src/projectData.js
@@ -1,7 +1,7 @@
 const projectData = [
-    {title: "Project 1", description: "Description of Project 1"},
-    {title: "Project 2", description: "Description of Project 2"},
-    {title: "Project 3", description: "Description of Project 3"}
+    {title: "Flight of the Bumblebee", description: "A fast paced some meant to imulate a bumblebee buzzing about", link: "https://www.youtube.com/watch?v=M93qXQWaBdE"},
+    {title: "Quarentine Interview", description: "A lo-fidelity song made during the start of the covid-19 pandemic right before an interview for my first full time job", link: "https://soundcloud.com/jamie-bryson-1/quarentine-interview?in=jamie-bryson-1/sets/lo-fi&si=5bd137b129aa4558983f658cd9c39b92&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"},
+    {title: "Tic-Tac-Toe", description: "A classic game made using basic JavaScript, HTML, and CSS knowledge", link: "https://github.com/jamieab98/fun-MultiplayerTicTacToe/blob/main/ticIndex.html"},
 ]
 
 export default projectData;
