06a5d5326b3d51ae6833832240e5c2bfc819e250
Jamie Bryson <jamieab98@gmail.com>
2025-10-20 16:28:37 -0400

    Create the userSeach state and the updateUserSearch function in SearchProject

--- src/components/SearchProject.jsx
+++ src/components/SearchProject.jsx
@@ -1,10 +1,17 @@
 import styles from "./SearchProject.module.css"
+import { useState } from "react"
 
 function SearchProject() {
+    const [userSearch, setUserSearch] = useState("");
+
+    const updateUserSearch = (e) => {
+        setUserSearch(e.target.value);
+    }
+
     return(
         <div className={styles.SearchProject}>
-            <form>
-                <input type="text" placeholder="Search Projects" className={styles.projectsearch}></input>
+            <form htmlFor="projectSearch">
+                <input type="text" placeholder="Search Projects" className={styles.projectsearch} id="projectSearch" value={userSearch} onChange={updateUserSearch}></input>
             </form>
         </div>
     )
